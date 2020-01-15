8399eb1c259b90bcce13469f12f71a38546d0e0a
JayenKerai <32294741+JayenKerai@users.noreply.github.com>
2020-01-15 15:13:30 +0000

    Add files via upload
    updated bubble sort to be generic

--- BubbleSort/src/com/sparta/jk/engineering50/BubbleSort.java
+++ BubbleSort/src/com/sparta/jk/engineering50/BubbleSort.java
@@ -5,14 +5,14 @@ package com.sparta.jk.engineering50;
 - Use sortAsc() for ascending order, and sortDesc for descending order
  */
 
-public class BubbleSort {
-    private int[] intArray; //contains the array to be sorted
+public class BubbleSort  <T extends Comparable<T>> {
+    private T[] intArray; //contains the array to be sorted
 
-    public BubbleSort(int[] intArray) {
+    public BubbleSort(T[] intArray) {
         this.intArray = intArray;
     }
 
-    public int[] sortAsc(){
+    public T[] sortAsc(){
         int pointer = 0; //points to current location in array
         while (pointer < intArray.length - 1){ //loop until the array is sorted (end of array reached)
             if (isBiggerThanNext(pointer)){ //checks if current pointer location is bigger than next location
@@ -25,7 +25,7 @@ public class BubbleSort {
         return intArray;
     }
 
-    public int[] sortDesc(){
+    public T[] sortDesc(){
         int pointer = 0; //points to current location in array
         while (pointer < intArray.length - 1){ //loop until the array is sorted (end of array reached)
             if (isSmallerThanNext(pointer)){ //checks if current pointer location is smaller than next location
@@ -39,17 +39,17 @@ public class BubbleSort {
     }
 
     private void swap(int i){ //swaps ints at i and i+1
-        int temp = intArray[i]; //stores int at i in temp
+        T temp = intArray[i]; //stores int at i in temp
         intArray[i] = intArray[i+1]; //puts i+1 where i is
         intArray[i+1] = temp; // puts stored int at i+1
     }
 
     private boolean isBiggerThanNext(int i){ //if i is greater than i+1, return true, else return false
-        return (intArray[i] > intArray[i+1]);
+        return (intArray[i].compareTo(intArray[i+1]) > 0 );
     }
 
     private boolean isSmallerThanNext(int i){ //if i is smaller than i+1, return true, else return false
-        return (intArray[i] < intArray[i+1]);
+        return (intArray[i].compareTo(intArray[i+1]) < 0 );
     }
 
 }
--- BubbleSort/src/com/sparta/jk/engineering50/Main.java
+++ BubbleSort/src/com/sparta/jk/engineering50/Main.java
@@ -5,13 +5,14 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] intArray = {1, 3, 4, 2, 6, 5, 2, 3, 10, 7, 9, 10, 9};
+        Integer[] intArray = {1, 3, 4, 2, 6, 5, 2, 3, 10, 7, 9, 10, 9};
         BubbleSort bubbleSort = new BubbleSort(intArray);
 
-        int[] sortedAsc = bubbleSort.sortDesc();
-        System.out.println(Arrays.toString(sortedAsc)); //prints ascending bubble-sorted intArray
 
-        int[] sortedDesc = bubbleSort.sortAsc();
+        Integer[] sortedAsc = (Integer[]) bubbleSort.sortAsc();
+        System.out.println(Arrays.toString(sortedAsc)); //prints ascending bubble-sorted intArray
+        System.out.println(Arrays.toString(intArray));
+        Integer[] sortedDesc = (Integer[]) bubbleSort.sortDesc();
         System.out.println(Arrays.toString(sortedDesc)); //prints descending bubble-sorted intArray
 
     }
